7734315e6f73df0c31c9293e6d9eb989a0f41bd6
yamankhan <yamankhanosu@gmail.com>
2012-09-28 12:00:26 -0400

    social stream ready to go

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Citysocial::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
+config.action_mailer.default_url_options = { :host => 'citybeach.herokuapp.com'}
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
